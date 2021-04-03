0c409eafcec47f97845b0762e28de3bba7a27060
freelancer-abdulfaizan <af95537@gmail.com>
2021-04-03 15:17:06 +0500

    first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">  
+    <div v-if="totalAnswer == questions.length">
+      <b-modal id="modal-1" no-close-on-backdrop title="Your Result" @ok="saveData">
+        <p class="my-4">Total Questions: <b>{{totalAnswer}}</b>!</p>
+        <p class="my-4">Total Correct Answer is: <b>{{correctAnswer}}</b>!</p>
+      </b-modal>
+    </div>
+    <Header :correctAnswer='correctAnswer' :totalAnswer="totalAnswer" />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col v-if="questions.length > 0" sm="6" offset="3">
+          <questionBox :increment='increment' :currentQuestion="questions[index]" :changeQuestion="changeQuestion"/>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import questionBox from './components/questionBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    questionBox
+  },
+  data(){
+    return {
+      questions: [],
+      index: 0,
+      correctAnswer: 0,
+      totalAnswer: 0
+    }
+  },
+  methods: {
+    saveData: function(){
+      window.location.reload()
+    },
+    changeQuestion: function(){
+      console.log('fffffffffffffff',this.totalAnswer);
+      if( this.index+1 != this.questions.length ){
+        this.index++;
+      }
+    },
+    increment: function(flag){
+      if(flag){
+        this.correctAnswer++;
+      }
+      this.totalAnswer++;
+    }
+  },
+  mounted: function(){
+    fetch('https://opentdb.com/api.php?amount=10&category=18&type=multiple',{
+      method:'get'
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(res => {
+      this.questions = res.results;      
+      console.log('fff',res.results);
+    })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+
+import { BootstrapVue, ModalPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 
+Vue.use(BootstrapVue)
+Vue.use(ModalPlugin)
 Vue.config.productionTip = false
 
 new Vue({
